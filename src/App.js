839db145d458e16b371547e949bd6b32da677b0c
Rory <rory.starks@gmail.com>
2020-01-27 13:23:36 -0800

    Added node version

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 style={...defaultStyle, 'font-size': '54px'}>Title</h1>
+        <h1 style={{...defaultStyle, 'font-size': '54px'}}>Title</h1>
         <Aggregate/>
         <Aggregate/>
         <Filter/>
